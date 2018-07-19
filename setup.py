dd3473acab1546fb8517178eb53586143a294797
Jim Hill <jim@dammitjim.co.uk>
2018-07-19 11:53:22 +0100

    Updating gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 from autocomplete import get_version
+
 setup(
     name = "django-autocomplete",
     packages = find_packages(),
@@ -10,6 +11,4 @@ setup(
     author = "Incuna Ltd",
     author_email = "admin@incuna.com",
     url = "http://incuna.com/",
-    # download_url = "http://chardet.feedparser.org/download/python3-chardet-1.0.1.tgz",
-    # long_description = """"""
 )
